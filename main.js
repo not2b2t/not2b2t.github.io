efce90b9d7709f21c68952f80b788522eea44a0c
not2b2t <143640645+not2b2t@users.noreply.github.com>
2023-10-29 15:20:10 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
-function handleCopy() {
-    const ip = "not2b2t.org";
-    var copied = "IP address copied to clipboard."
-    navigator.clipboard.writeText(ip);
-    alert("IP copied to clipboard.")
-
+function handleClick() {
+  const ip = "not2b2t.org";
+  const copied = document.getElementById("success");
+  navigator.clipboard.writeText(ip);
+  copied.innerHTML = "IP copied to clipboard.";
 }
 
-const about = "not2b2t is a minecraft anarchy server with a world that never resets and free for all pvp environment.";
-document.getElementById("about").innerText = about;
+function joinDiscord() {
+  window.open("https://not2b2t.org/discord");
+}
+
 
 const opened = new Date('2022-11-19');
 const today = new Date();
 const age = today - opened;
 const nigger = Math.floor(age / (1000 * 60 * 60 * 24));
-document.getElementById("age").innerText = `the world is ${nigger} days old and over 30 gigabytes in size with over 350 players visiting.`;
+document.getElementById("age").innerHTML = `the world is <strong>${nigger} days</strong> old and it's size is over <strong>30 gigabytes</strong><br> with over <strong>350 people</strong> visiting.`;
+document.getElementById("days").innerHTML = `World age:&nbsp;&nbsp; ${nigger} days`;
 
 
 const api = "https://api.mcsrvstat.us/3/not2b2t.org";
@@ -21,8 +23,29 @@ fetch(api)
   .then(response => response.json())
   .then(data => {
     const onlinePlayers = data.players.online;
-    document.getElementById("players").innerText = `Join ${onlinePlayers} other players now to be a part of the not2b2t history!`;
-  })
+    document.getElementById("playerCount").innerText = `${onlinePlayers} players online. Join using IP not2b2t.org`;
+    document.getElementById("navbarCount").innerText = `${onlinePlayers}/50 players online`;
+})
   .catch(error => {
     console.error('Error fetching data:', error);
   });
+
+
+const discord = document.getElementById("online");
+
+const apiUrl = "https://discord.com/api/guilds/1047850502760710285/widget.json";
+
+fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        if (data && data.presence_count !== undefined) {
+            const onlineDiscord = data.presence_count;
+            discord.textContent = `Online Members: ${onlineDiscord}`;
+        } else {
+            discord.textContent = "Could not load online users";
+        }
+    })
+    .catch(error => {
+        console.error(error);
+        discord.textContent = "Could not load online users";
+    });
